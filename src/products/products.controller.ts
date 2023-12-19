a45bba25277220b78c68cd05377c9fe20568cefd
IKIM <Ecris ton email ici, en gardant les quotes>
2023-12-19 12:55:21 +0100

    update yml

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,51 +1,67 @@
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Patch, Post, Put } from "@nestjs/common";
-import { ProductsService } from "./products.service";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-    constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) {}
 
-    @Get()
-    getAllProducts() {
-        return this.productsService.getProducts();
-    }
+  @Get()
+  getAllProducts() {
+    return this.productsService.getProducts();
+  }
 
-    @Get(':id')
-    getProduct(@Param('id') prodId: string) {
-        return this.productsService.getSingleProduct(prodId);
-    }
+  @Get(':id')
+  getProduct(@Param('id') prodId: string) {
+    return this.productsService.getSingleProduct(prodId);
+  }
 
-    @Put(':id')
-    updateProduct(
+  @Patch(':id')
+  updateProduct(
     @Param('id') prodId: string,
     @Body('title') prodTitle: string,
     @Body('description') prodDescription: string,
     @Body('price') prodPrice: number,
-    ): { message: string } {
+  ): { message: string } {
     try {
-        this.updateProduct(prodId, prodTitle, prodDescription, prodPrice);
-        return { message: 'Product updated successfully.' };
+      this.updateProduct(prodId, prodTitle, prodDescription, prodPrice);
+      return { message: 'Product updated successfully.' };
     } catch (error) {
-        // You can customize the error handling based on your requirements.
-        console.error(error.message);
-        throw new HttpException('Failed to update product.', HttpStatus.INTERNAL_SERVER_ERROR);
+      // You can customize the error handling based on your requirements.
+      console.error(error.message);
+      throw new HttpException(
+        'Failed to update product.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-    }
+  }
 
+  @Post()
+  addProduct(
+    @Body('title') productTitle: string,
+    @Body('description') productDescription: string,
+    @Body('price') productPrice: number,
+  ) {
+    const generatedId = this.productsService.addProduct(
+      productTitle,
+      productDescription,
+      productPrice,
+    );
 
-    @Post()
-    addProduct(
-    @Body('title') productTitle: string, 
-    @Body('description') productDescription: string, 
-    @Body('price') productPrice: number) {
+    return { id: generatedId };
+  }
 
-    const generatedId = this.productsService.addProduct(productTitle, productDescription, productPrice);  
-    
-    return {id: generatedId};
-    }
-
-    @Delete(':id')
-    deleteProduct(@Param('id') prodId: string) {
-        this.productsService.deleteProduct(prodId);
-    }
+  @Delete(':id')
+  deleteProduct(@Param('id') prodId: string) {
+    this.productsService.deleteProduct(prodId);
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,67 +1,69 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
-import { Product } from "./product.model";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from './product.model';
 
 @Injectable()
 export class ProductsService {
-    private products: Product[] = [];
+  private products: Product[] = [];
 
-    getProducts() {
+  getProducts() {
+    // Copy object in a new array
+    return [...this.products];
+  }
 
-        // Copy object in a new array
-        return [...this.products];
+  getSingleProduct(prodId: string) {
+    const product = this.products.find((prod) => prod.id === prodId);
+
+    if (!product) {
+      throw new NotFoundException('Could not find product.');
     }
+    // Copy in a new object
+    return { ...product };
+  }
 
-    getSingleProduct(prodId: string) {
-        const product = this.products.find(prod => prod.id === prodId);
+  updateProduct(
+    prodId: string,
+    title: string,
+    description: string,
+    price: number,
+  ): void {
+    const productIndex = this.products.findIndex((prod) => prod.id === prodId);
 
-        if (!product) {
-            throw new NotFoundException('Could not find product.');
-        }
-        // Copy in a new object
-        return {... product};
+    if (productIndex !== -1) {
+      const updatedProduct = { ...this.products[productIndex] };
+
+      if (title !== undefined && title !== null) {
+        updatedProduct.title = title;
+      }
+
+      if (description !== undefined && description !== null) {
+        updatedProduct.description = description;
+      }
+
+      if (price !== undefined && price !== null) {
+        updatedProduct.price = price;
+      }
+
+      this.products[productIndex] = { ...updatedProduct };
+    } else {
+      // Handle the case where the product with the specified ID was not found.
+      // You can throw an exception or handle it according to your application logic.
+      console.error(`Product with ID ${prodId} not found.`);
     }
+  }
 
-    updateProduct(prodId: string, title: string, description: string, price: number): void {
-        const productIndex = this.products.findIndex(prod => prod.id === prodId);
-    
-        if (productIndex !== -1) {
-            const updatedProduct = { ...this.products[productIndex] };
-    
-            if (title !== undefined && title !== null) {
-                updatedProduct.title = title;
-            }
-    
-            if (description !== undefined && description !== null) {
-                updatedProduct.description = description;
-            }
-    
-            if (price !== undefined && price !== null) {
-                updatedProduct.price = price;
-            }
-    
-            this.products[productIndex] = { ...updatedProduct };
-        } else {
-            // Handle the case where the product with the specified ID was not found.
-            // You can throw an exception or handle it according to your application logic.
-            console.error(`Product with ID ${prodId} not found.`);
-        }
+  addProduct(title: string, description: string, price: number) {
+    const prodId = Math.random().toString();
+    const newProduct = new Product(prodId, title, description, price);
+    this.products.push(newProduct);
+
+    return prodId;
+  }
+
+  deleteProduct(prodId: string): void {
+    const index = this.products.findIndex((prod) => prod.id === prodId);
+
+    if (index !== -1) {
+      this.products.splice(index, 1);
     }
-    
-
-    addProduct(title: string, description: string, price: number) {
-        const prodId = Math.random().toString();
-        const newProduct = new Product(prodId, title, description, price);
-        this.products.push(newProduct);
-
-        return prodId;
-    }
-
-    deleteProduct(prodId: string): void {
-        const index = this.products.findIndex(prod => prod.id === prodId);
-    
-        if (index !== -1) {
-            this.products.splice(index, 1);
-        }
-    }
-    
+  }
 }
